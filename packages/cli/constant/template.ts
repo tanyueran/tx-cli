0e610a47ed739eef46a4483a26d9afee71dbb9e2
tanxin <txloveu@163.com>
2025-08-12 08:49:31 +0800

    fix: 简化不必要的模板

--- packages/cli/constant/template.ts
+++ packages/cli/constant/template.ts
@@ -10,20 +10,10 @@ export const ProjectTemplateData = [
     label: "webpack + vue + ts项目",
     value: ProjectTemplateEnum.WebpackVueTs,
     path: "../packages/template/project/vue/webpack-vue-ts",
-  }
+  },
 ];
 
 export const ModuleTemplateData = [
-  {
-    label: "vue-ts的页面模板",
-    value: ModuleTemplateEnum.VueTsPageTemplate,
-    path: "../template/module/vue/vue-ts-page-template",
-  },
-  {
-    label: "vue-ts的详情模板",
-    value: ModuleTemplateEnum.VueTsDetailTemplate,
-    path: "../template/module/vue/vue-ts-detail-template",
-  },
   {
     label: "vue-ts的空模板",
     value: ModuleTemplateEnum.VueTsEmpty,
